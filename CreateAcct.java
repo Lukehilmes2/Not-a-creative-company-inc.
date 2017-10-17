6cd2fec73ade82ca429d79b77766ef06adf5e35b
Max Well <thibeaumax@gmail.com>
2017-10-17 14:40:48 -0600

    stuff

--- CreateAcct.java
+++ CreateAcct.java
@@ -14,134 +14,55 @@ import javax.swing.*;
 public class CreateAcct extends JPanel {
 
 	
-	    JTextField Name;
-		JTextField StartingBal;
-		JTextField FName, LName, Password;
-	    JLabel Acct;
-	    JButton Create;
-	    String File = "login.txt";
-	    
+    JTextField Name;
+	JTextField FName, LName, Password;
+	JLabel lblName, lblFirstName, lblLastName, lblPassword;
+    JLabel Acct;
+    JButton Create;
+    String File = "login.txt";
 
+    public CreateAcct() 
+    {
+    	Name = new JTextField("Name");
+    	FName = new JTextField("First Name");
+    	LName = new JTextField("Last Name");
+    	Password = new JTextField("Password");
+    	Create  = new JButton("Create Account");
+    	Acct = new JLabel();
+    	Acct.setText("Create a new Account!");
+    	lblName = new JLabel("Username: ");
+    	lblFirstName = new JLabel("First name: ");
+    	lblLastName = new JLabel("Last name: ");
+    	lblPassword = new JLabel("Password: ");
+    	
+    	add(Acct);
+    	add(lblName);
+    	add(Name);
+    	add(lblFirstName);
+    	add(FName);
+    	add(lblLastName);
+    	add(LName);
+    	add(lblPassword);
+    	add(Password);
+    	add(Create);
+    	Create.addActionListener(new ButtonListener());
+    }
+    private class ButtonListener implements ActionListener{
 
-	    public CreateAcct() 
-	    {
-	    	Name = new JTextField("Name");
-	    	StartingBal = new JTextField("Starting Balance");
-	    	FName = new JTextField("First Name");
-	    	LName = new JTextField("Last Name");
-	    	Password = new JTextField("Password");
-	    	Create  = new JButton("Create Account");
-	    	
-	    	Acct = new JLabel();
-	    	Acct.setText("Create a new Account!");
-	    	
-	    	add(Acct);
-	    	add(Name);
-	    	add(StartingBal);
-	    	add(FName);
-	    	add(LName);
-	    	add(Password);
-	    	add(Create);
-	    	Create.addActionListener(new ButtonListener());
-	    	
-	    	Name.addFocusListener(new FocusListener(){
-	    		@Override
-	    		public void focusGained(FocusEvent e){
-	    			if(e.getSource().equals(Name)){
-	    			Name.setText("Name");
-	    			}
-	    			
-	    		}
-
-				@Override
-				public void focusLost(FocusEvent arg0) {
-					
-					
-				}
-	    		});
-	    	FName.addFocusListener(new FocusListener(){
-	    		@Override
-	    		public void focusGained(FocusEvent e){
-	    			if(e.getSource().equals(FName)){
-	    			FName.setText("");
-	    			}
-	    			
-	    		}
-
-				@Override
-				public void focusLost(FocusEvent arg0) {
-					
-					
-				}
-	    		});
-	    	LName.addFocusListener(new FocusListener(){
-	    		@Override
-	    		public void focusGained(FocusEvent e){
-	    			if(e.getSource().equals(LName)){
-	    			LName.setText("");
-	    			}
-	    			
-	    		}
-
-				@Override
-				public void focusLost(FocusEvent arg0) {
-					
-					
-				}
-	    		});
-	    	Password.addFocusListener(new FocusListener(){
-	    		@Override
-	    		public void focusGained(FocusEvent e){
-	    			if(e.getSource().equals(Password)){
-	    			Password.setText("");
-	    			}
-	    			
-	    		}
-
-				@Override
-				public void focusLost(FocusEvent arg0) {
-					
-					
-				}
-	    		});
-	    	StartingBal.addFocusListener(new FocusListener(){
-	    		@Override
-	    		public void focusGained(FocusEvent e){
-	    			if(e.getSource().equals(StartingBal)){
-	    			StartingBal.setText("");
-	    			}
-	    			
-	    		}
-
-				@Override
-				public void focusLost(FocusEvent arg0) {
-					
-					
-				}
-	    		});
-	    	}
-
-	    private class ButtonListener implements ActionListener{
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				String newAcct = (Name.getText()+","+Password.getText());
-
-				try {
-					BufferedWriter writer = new BufferedWriter(new FileWriter(File));
-					writer.write(newAcct);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-			}
-	    	
-	    	
-	    	
-	    }
-	    }
+    	public void actionPerformed(ActionEvent arg0) {
+			String newAcct = (Name.getText()+","+Password.getText());
+			BufferedWriter writer = null;
+			try {
+				writer = new BufferedWriter(new FileWriter(File, true));
+				PrintWriter out = new PrintWriter(writer);
+				out.println(newAcct.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+		}    	
+    }
+}
 
 	
 
