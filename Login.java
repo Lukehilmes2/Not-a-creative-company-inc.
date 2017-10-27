fe030077f21647d413a066819f20f81b3fda6d28
Luke <lukehilmes2@gmail.com>
2017-10-26 21:29:46 -0600

    Changed the login appearance

--- Login.java
+++ Login.java
@@ -3,40 +3,71 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+import javax.swing.border.*;
 class Login extends JPanel {
 
   private JTextField username;
-  private JTextField password;
+  private JPasswordField password;
   private JLabel lblUsername;
   private JLabel lblPassword;
+  private JLabel label;
   private JButton login;
-  private int txtFieldLength = 10;
+  private int txtFieldLength = 20;
   private MainPanel panel;
   private JLabel lblBadLogin;
+
+
   public Login (MainPanel panel) {
 
-	this.panel = panel;
-    username = new JTextField(txtFieldLength);
-    password = new JTextField(txtFieldLength);
+	  this.panel = panel;
+    setLayout(new BorderLayout());
+    JPanel panel1 = new JPanel(new GridBagLayout());
+    GridBagConstraints cs = new GridBagConstraints();
+
+    cs.fill = GridBagConstraints.HORIZONTAL;
+
     lblUsername = new JLabel("Username: ");
+    cs.gridx = 0;
+    cs.gridy = 0;
+    cs.gridwidth = 1;
+    panel1.add(lblUsername, cs);
+
+    username = new JTextField(20);
+    cs.gridx = 1;
+    cs.gridy = 0;
+    cs.gridwidth = 2;
+    panel1.add(username, cs);
+
     lblPassword = new JLabel("Password: ");
+    cs.gridx = 0;
+    cs.gridy = 1;
+    cs.gridwidth = 1;
+    panel1.add(lblPassword, cs);
+
+    password = new JPasswordField(20);
+    cs.gridx = 1;
+    cs.gridy = 1;
+    cs.gridwidth = 2;
+    panel1.add(password, cs);
+    panel1.setBorder(new LineBorder(Color.GRAY));
+
+    login = new JButton("Login");
     lblBadLogin = new JLabel("");
-    login = new JButton("login");
-    add(lblUsername);
-    add(username);
-    add(lblPassword);
-    add(password);
-    add(login);
-    add(lblBadLogin);
     login.addActionListener(new ButtonListener());
+    JPanel bp = new JPanel();
+    bp.add(login);
+    bp.add(lblBadLogin);
+    add(panel1, BorderLayout.CENTER);
+    add(bp, BorderLayout.PAGE_END);
+
 
   }
 
   private class ButtonListener implements ActionListener{
 
     public void actionPerformed(ActionEvent evt){
-	
-	if (username.getText().equals("CSAdmin") && password.getText().equals("CSCI323")) {
+
+	if (username.getText().equalsIgnoreCase("CSAdmin") && password.getText().equalsIgnoreCase("CSCI323")) {
 		panel.switchPanel("InitialView");
     	lblBadLogin.setText("");
     	username.setText("");
@@ -63,7 +94,7 @@ class Login extends JPanel {
         	return;
         }
       }*/
-      lblBadLogin.setText("bad password");
+      lblBadLogin.setText("<html>Bad Details!<br>Try Again</html>");
 	}
   }
 }
