7ed9518e0dacd2c4ffc2e181c440ea47adb5b2fa
Luke <lukehilmes2@gmail.com>
2017-11-30 10:41:04 -0700

    stuff

--- MainPanel.java
+++ MainPanel.java
@@ -37,6 +37,7 @@ public class MainPanel extends JPanel{
 		add(pnlInitialView, "InitialView");
 		add(pnlCreateAcct, "CreateAcct");
 		add(pnlViewAcct, "ViewAcct");
+		add(pnlModifyAcct,"ModifyAcct");
 		add(pnlAddTrans, "AddTrans");
 		add(pnlBenefits, "Benefits");
 		add(pnlTransactionView, "TransactionView");
