9977b049b0846b5ab7a1bdb60815736bbd597684
Max Well <thibeaumax@gmail.com>
2017-12-04 15:20:07 -0700

    to be popped

--- MainPanel.java
+++ MainPanel.java
@@ -1,5 +1,7 @@
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -12,6 +14,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -28,7 +31,7 @@ public class MainPanel extends JPanel {
 	private final String companyName = "Developed by NACC inc.";
 	private final double minutesAutoLogOut = 10;
 	private Timer timer;
-
+	
 	public MainPanel() {
 
 		cards = new CardLayout();
